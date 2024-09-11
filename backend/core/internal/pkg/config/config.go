ee52ee302b41635669e5d662906c34b5865e70f8
Zunayed <zunayedology@gmail.com>
2024-09-11 15:33:21 +0600

    Postgres connection set up

---
+++ backend/core/internal/pkg/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+func ConnectPostgres() *gorm.DB {
+	// Define your connection string here
+	dsn := "host=localhost user=postgres password=280899 dbname=demure_core port=5432 sslmode=disable TimeZone=Asia/Dhaka"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	return db
+}
