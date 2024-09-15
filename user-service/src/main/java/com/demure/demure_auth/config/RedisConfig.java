f0beb734981bd752022fe62acf8f6b2fdefdf4e2
Zunayed <zunayedology@gmail.com>
2024-09-15 13:56:22 +0600

    Redis configured

---
+++ user-service/src/main/java/com/demure/demure_auth/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.demure.demure_auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return new StringRedisTemplate(factory);
+    }
+}
